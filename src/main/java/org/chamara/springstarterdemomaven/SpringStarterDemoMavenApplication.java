952f3efc76cc622aa6c796677d8b912fef5b4cd6
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-02 16:08:05 +0530

    initial commit

---
+++ src/main/java/org/chamara/springstarterdemomaven/SpringStarterDemoMavenApplication.java
@@ -0,0 +1,13 @@
+package org.chamara.springstarterdemomaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterDemoMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStarterDemoMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/chamara/springstarterdemomaven/SpringStarterDemoMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chamara.springstarterdemomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarterDemoMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
