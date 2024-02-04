a8fd7241e493bf2e010ead2a04ad7580278c2557
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-04 21:16:14 +0530

    Refactor CustomerJPADataAccessService implementation

--- src/main/java/org/chamara/springstarterdemomaven/customer/CustomerJPADataAccessService.java
+++ src/main/java/org/chamara/springstarterdemomaven/customer/CustomerJPADataAccessService.java
@@ -1,2 +1,26 @@
-package org.chamara.springstarterdemomaven.customer;public class CustomerJPADataAccessService {
+package org.chamara.springstarterdemomaven.customer;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository("jpa")
+public class CustomerJPADataAccessService implements CustomerDoa {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerJPADataAccessService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Customer> getCustomerById(Integer id) {
+        return Optional.empty();
+    }
 }
