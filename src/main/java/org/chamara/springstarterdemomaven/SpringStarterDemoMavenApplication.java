f3d6c520e128dffbe4e5a056687d1512b92b2b71
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-04 12:35:45 +0530

    feat: adding greeting response

--- src/main/java/org/chamara/springstarterdemomaven/SpringStarterDemoMavenApplication.java
+++ src/main/java/org/chamara/springstarterdemomaven/SpringStarterDemoMavenApplication.java
@@ -2,12 +2,27 @@ package org.chamara.springstarterdemomaven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class SpringStarterDemoMavenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringStarterDemoMavenApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStarterDemoMavenApplication.class, args);
+    }
 
+    @GetMapping("/greet")
+    public GreetResponse greet() {
+        return new GreetResponse(new Person("Chamara", 30), List.of("Java", "Kotlin", "Spring"));
+    }
+
+    record Person(String name, int age) {
+    }
+
+    record GreetResponse(Person person, List<String> favouriteProgrammingLanguages) {
+    }
 }
