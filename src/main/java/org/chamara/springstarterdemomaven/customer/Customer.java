0d99449630259dc1786d06745296a49f10e22400
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-04 21:15:29 +0530

    Add ID generation strategy and column constraints to Customer entity

--- src/main/java/org/chamara/springstarterdemomaven/customer/Customer.java
+++ src/main/java/org/chamara/springstarterdemomaven/customer/Customer.java
@@ -1,13 +1,24 @@
 package org.chamara.springstarterdemomaven.customer;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
 public class Customer {
-
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            generator = "customer_id_sequence",
+            strategy = GenerationType.SEQUENCE
+    )
     private Integer id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private Integer age;
 
     public Customer(Integer id, String name, String email, Integer age) {
@@ -15,6 +26,16 @@ public class Customer {
         this.name = name;
         this.email = email;
         this.age = age;
+    }
+
+    public Customer(String name, String email, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Customer() {
+
     }
 
     public Integer getId() {
