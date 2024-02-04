3a9bfe672051fdd35b7c5b05573e9d0512212cb7
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-04 21:15:49 +0530

    Rename CustomerDOA interface to CustomerDoa

--- src/main/java/org/chamara/springstarterdemomaven/customer/CustomerDoa.java
+++ src/main/java/org/chamara/springstarterdemomaven/customer/CustomerDoa.java
@@ -3,7 +3,7 @@ package org.chamara.springstarterdemomaven.customer;
 import java.util.List;
 import java.util.Optional;
 
-interface CustomerDOA {
+interface CustomerDoa {
     List<Customer> getCustomers();
 
     Optional<Customer> getCustomerById(Integer id);
