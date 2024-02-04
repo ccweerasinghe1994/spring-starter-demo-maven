452d3ab4fe17d115b94105360679d564e30a05b7
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-04 15:29:15 +0530

    Refactor greet method to handle optional name parameter

--- src/main/java/org/chamara/springstarterdemomaven/SpringStarterDemoMavenApplication.java
+++ src/main/java/org/chamara/springstarterdemomaven/SpringStarterDemoMavenApplication.java
@@ -1,11 +1,13 @@
 package org.chamara.springstarterdemomaven;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -16,8 +18,14 @@ public class SpringStarterDemoMavenApplication {
     }
 
     @GetMapping("/greet")
-    public GreetResponse greet() {
-        return new GreetResponse(new Person("Chamara", 30), List.of("Java", "Kotlin", "Spring"));
+    public GreetResponse greet(@RequestParam(value = "userName", required = false) String name) {
+        Optional<String> optionalName = Optional.ofNullable(name);
+        String nameValue = optionalName.orElseGet(() -> "No Name Provided");
+        // let's remove the double quotes from the name
+        nameValue = nameValue.replaceAll("\"", "");
+        // check if the name is empty
+        nameValue = nameValue.isBlank() ? "Empty String" : nameValue;
+        return new GreetResponse(new Person(nameValue, 30), List.of("Java", "Kotlin", "Spring"));
     }
 
     record Person(String name, int age) {
