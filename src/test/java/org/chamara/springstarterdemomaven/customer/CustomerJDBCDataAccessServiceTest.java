19a7e70e80a989e487e5494b6d2bb8954b98626a
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-08 10:11:17 +0530

    Add CustomerJDBCDataAccessServiceTest class

---
+++ src/test/java/org/chamara/springstarterdemomaven/customer/CustomerJDBCDataAccessServiceTest.java
@@ -0,0 +1,60 @@
+package org.chamara.springstarterdemomaven.customer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CustomerJDBCDataAccessServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void itShouldSelectAllCustomers() {
+        // given
+        // when
+        // then
+    }
+
+    @Test
+    void itShouldSelectCustomerById() {
+        // given
+        // when
+        // then
+    }
+
+    @Test
+    void itShouldInsertCustomer() {
+        // given
+        // when
+        // then
+    }
+
+    @Test
+    void itShouldExistsCustomerWithEmail() {
+        // given
+        // when
+        // then
+    }
+
+    @Test
+    void itShouldDeleteCustomerById() {
+        // given
+        // when
+        // then
+    }
+
+    @Test
+    void itShouldExistsCustomerById() {
+        // given
+        // when
+        // then
+    }
+
+    @Test
+    void itShouldUpdateCustomer() {
+        // given
+        // when
+        // then
+    }
+}
