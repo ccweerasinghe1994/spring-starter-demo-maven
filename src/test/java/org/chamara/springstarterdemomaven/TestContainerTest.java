3c1be6459739fd02fe608cf6761e64185d437b82
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-08 10:21:48 +0530

    Remove unused imports and setUp method

--- src/test/java/org/chamara/springstarterdemomaven/TestContainerTest.java
+++ src/test/java/org/chamara/springstarterdemomaven/TestContainerTest.java
@@ -1,24 +1,10 @@
 package org.chamara.springstarterdemomaven;
 
-import org.chamara.springstarterdemomaven.customer.CustomerJDBCDataAccessService;
-import org.chamara.springstarterdemomaven.customer.CustomerRawMapper;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 
 public class TestContainerTest extends AbstractTestContainer {
-
-    private CustomerJDBCDataAccessService underTest;
-
-
-    private final CustomerRawMapper customerRawMapper = new CustomerRawMapper();
-
-    @BeforeEach
-    void setUp() {
-        underTest = new CustomerJDBCDataAccessService(new JdbcTemplate(), customerRawMapper);
-    }
 
     @Test
     void canStartPostgresDB() {
