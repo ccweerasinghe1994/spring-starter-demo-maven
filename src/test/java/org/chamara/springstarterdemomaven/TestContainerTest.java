1d62654e381c36f66581746883cb55ae8507161d
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-08 09:50:55 +0530

    Add dynamic properties for PostgreSQL container

--- src/test/java/org/chamara/springstarterdemomaven/TestContainerTest.java
+++ src/test/java/org/chamara/springstarterdemomaven/TestContainerTest.java
@@ -3,6 +3,8 @@ package org.chamara.springstarterdemomaven;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -21,6 +23,13 @@ public class TestContainerTest {
             .withUsername(POSTGRES_USERNAME)
             .withPassword(POSTGRES_PASSWORD)
             .withDatabaseName(POSTGRES_DB_NAME);
+
+    @DynamicPropertySource
+    static void setPostgresProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
 
     @Test
     void canStartPostgresDB() {
