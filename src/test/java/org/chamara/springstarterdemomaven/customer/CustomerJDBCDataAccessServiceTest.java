d5b22e4dd4a9b8d8b1024db7e8890e2f355075d0
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-08 13:35:27 +0530

    Add test case to select customer by ID

--- src/test/java/org/chamara/springstarterdemomaven/customer/CustomerJDBCDataAccessServiceTest.java
+++ src/test/java/org/chamara/springstarterdemomaven/customer/CustomerJDBCDataAccessServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 class CustomerJDBCDataAccessServiceTest extends AbstractTestContainer {
@@ -36,8 +37,25 @@ class CustomerJDBCDataAccessServiceTest extends AbstractTestContainer {
     @Test
     void itShouldSelectCustomerById() {
         // given
+        String emailAddress = FAKER.internet().emailAddress() + "-" + UUID.randomUUID();
+        Customer customer = new Customer(FAKER.name().firstName(), emailAddress, FAKER.number().numberBetween(0, 100));
+        underTest.insertCustomer(customer);
         // when
+        Long customerId = underTest.selectAllCustomers().stream()
+                .filter(c -> c.getEmail().equals(emailAddress))
+                .findFirst()
+                .map(Customer::getId)
+                .orElseThrow();
+
+        Optional<Customer> customerById = underTest.selectCustomerById(customerId);
+
         // then
+        Assertions.assertTrue(customerById.isPresent());
+        Assertions.assertEquals(customer.getEmail(), customerById.get().getEmail());
+        Assertions.assertEquals(customer.getName(), customerById.get().getName());
+        Assertions.assertEquals(customer.getAge(), customerById.get().getAge());
+
+
     }
 
     @Test
