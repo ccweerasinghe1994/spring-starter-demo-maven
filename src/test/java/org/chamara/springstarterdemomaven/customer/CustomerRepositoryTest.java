a1baca14471c1a19c128fd2568b4cff44f456991
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-09 06:22:53 +0530

    Add CustomerRepositoryTest class

---
+++ src/test/java/org/chamara/springstarterdemomaven/customer/CustomerRepositoryTest.java
@@ -0,0 +1,25 @@
+package org.chamara.springstarterdemomaven.customer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CustomerRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void itShouldExistsCustomerByEmail() {
+        // given
+        // when
+        // then
+    }
+
+    @Test
+    void itShouldExistsCustomerById() {
+        // given
+        // when
+        // then
+    }
+}
