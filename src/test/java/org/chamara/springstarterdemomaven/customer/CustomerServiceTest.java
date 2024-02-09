16fd50347f28cbab7f165cbedf29c29831f93bc0
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-09 20:36:58 +0530

    Add CustomerUpdateRequest and implement updateCustomerById method

--- src/test/java/org/chamara/springstarterdemomaven/customer/CustomerServiceTest.java
+++ src/test/java/org/chamara/springstarterdemomaven/customer/CustomerServiceTest.java
@@ -1,6 +1,7 @@
 package org.chamara.springstarterdemomaven.customer;
 
 import org.chamara.springstarterdemomaven.exception.DuplicateResourceException;
+import org.chamara.springstarterdemomaven.exception.RequestValidationException;
 import org.chamara.springstarterdemomaven.exception.ResourceNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,7 +20,7 @@ class CustomerServiceTest {
     @Mock
     private CustomerDoa customerDoa;
     private CustomerService underTest;
-    
+
     @BeforeEach
     void setUp() {
         underTest = new CustomerService(customerDoa);
@@ -99,23 +100,155 @@ class CustomerServiceTest {
     }
 
     @Test
-    void itShouldThrowWhenIdIsNotFound() {
-
+    void itWillThrowWhenIdIsNotFound() {
         // when
         when(customerDoa.existsCustomerById(1L)).thenReturn(false);
         // then
         assertThrows(ResourceNotFoundException.class, () -> underTest.deleteCustomerById(1L));
-
+        verify(customerDoa, never()).deleteCustomerById(1L);
     }
 
     @Test
     void itShouldUpdateCustomerById() {
         // given
-        Customer customer = new Customer("name", "email", 12);
-        CustomerUpdateRequest customerUpdateRequest = new CustomerUpdateRequest("name", "email", 12);
+        Long id = 1L;
+        String actualName = "Updated Name";
+        String actualEmail = "updated@gmail.com";
+        Integer actualAge = 22;
+
+        CustomerUpdateRequest update = new CustomerUpdateRequest(actualName, actualEmail, actualAge);
+
+        Customer existingCustomer = new Customer("Old Name", "old@gmail.com", 21);
+        when(customerDoa.selectCustomerById(id)).thenReturn(Optional.of(existingCustomer));
+        when(customerDoa.existsCustomerWithEmail(actualEmail)).thenReturn(false);
+
         // when
-        underTest.updateCustomerById(1L, customerUpdateRequest);
+        underTest.updateCustomerById(id, update);
+
         // then
-        verify(customerDoa).updateCustomer(customer);
+        ArgumentCaptor<Customer> customerArgumentCaptor = ArgumentCaptor.forClass(Customer.class);
+        verify(customerDoa).updateCustomer(customerArgumentCaptor.capture());
+
+        Customer capturedCustomer = customerArgumentCaptor.getValue();
+        assertEquals(actualName, capturedCustomer.getName());
+        assertEquals(actualEmail, capturedCustomer.getEmail());
+        assertEquals(actualAge, capturedCustomer.getAge());
+    }
+
+    @Test
+    void itShouldOnlyUpdateNameUpdateCustomerById() {
+        // given
+        Long id = 1L;
+        String actualName = "Updated Name";
+
+        CustomerUpdateRequest update = new CustomerUpdateRequest(actualName, null, null);
+
+        Customer existingCustomer = new Customer("Old Name", "old@gmail.com", 21);
+        when(customerDoa.selectCustomerById(id)).thenReturn(Optional.of(existingCustomer));
+
+        // when
+        underTest.updateCustomerById(id, update);
+
+        // then
+        ArgumentCaptor<Customer> customerArgumentCaptor = ArgumentCaptor.forClass(Customer.class);
+        verify(customerDoa).updateCustomer(customerArgumentCaptor.capture());
+
+        Customer capturedCustomer = customerArgumentCaptor.getValue();
+        assertEquals(actualName, capturedCustomer.getName());
+        assertEquals(existingCustomer.getEmail(), capturedCustomer.getEmail());
+        assertEquals(existingCustomer.getAge(), capturedCustomer.getAge());
+    }
+
+    @Test
+    void itShouldOnlyUpdateAgeUpdateCustomerById() {
+        // given
+        Long id = 1L;
+        Integer actualAge = 22;
+        CustomerUpdateRequest update = new CustomerUpdateRequest(null, null, actualAge);
+
+        Customer existingCustomer = new Customer("Old Name", "old@gmail.com", 21);
+        when(customerDoa.selectCustomerById(id)).thenReturn(Optional.of(existingCustomer));
+
+        // when
+        underTest.updateCustomerById(id, update);
+
+        // then
+        ArgumentCaptor<Customer> customerArgumentCaptor = ArgumentCaptor.forClass(Customer.class);
+        verify(customerDoa).updateCustomer(customerArgumentCaptor.capture());
+
+        Customer capturedCustomer = customerArgumentCaptor.getValue();
+        assertEquals(existingCustomer.getName(), capturedCustomer.getName());
+        assertEquals(existingCustomer.getEmail(), capturedCustomer.getEmail());
+        assertEquals(actualAge, capturedCustomer.getAge());
+    }
+
+    @Test
+    void itShouldOnlyUpdateEmailUpdateCustomerById() {
+        // given
+        Long id = 1L;
+        String actualEmail = "updated@gmail.com";
+        CustomerUpdateRequest update = new CustomerUpdateRequest(null, actualEmail, null);
+
+        Customer existingCustomer = new Customer("Old Name", "old@gmail.com", 21);
+        when(customerDoa.selectCustomerById(id)).thenReturn(Optional.of(existingCustomer));
+        when(customerDoa.existsCustomerWithEmail(actualEmail)).thenReturn(false);
+        // when
+        underTest.updateCustomerById(id, update);
+
+        // then
+        ArgumentCaptor<Customer> customerArgumentCaptor = ArgumentCaptor.forClass(Customer.class);
+        verify(customerDoa).updateCustomer(customerArgumentCaptor.capture());
+
+        Customer capturedCustomer = customerArgumentCaptor.getValue();
+        assertEquals(existingCustomer.getName(), capturedCustomer.getName());
+        assertEquals(actualEmail, capturedCustomer.getEmail());
+        assertEquals(existingCustomer.getAge(), capturedCustomer.getAge());
+    }
+
+    @Test
+    void itWillThrowWhenEmailAlreadyExistsUpdateCustomerById() {
+        // given
+        Long id = 1L;
+        String actualName = "Updated Name";
+        String actualEmail = "updated@gmail.com";
+        Integer actualAge = 22;
+
+//        CustomerUpdateRequest update = mock(CustomerUpdateRequest.class);
+//        when(update.actualName()).thenReturn(actualName);
+//        when(update.actualEmail()).thenReturn(actualEmail);
+//        when(update.actualAge()).thenReturn(actualAge);
+        CustomerUpdateRequest update = new CustomerUpdateRequest(actualName, actualEmail, actualAge);
+
+        Customer existingCustomer = new Customer("Old Name", "old@gmail.com", 21);
+        when(customerDoa.selectCustomerById(id)).thenReturn(Optional.of(existingCustomer));
+        when(customerDoa.existsCustomerWithEmail(actualEmail)).thenReturn(true);
+
+        // when
+        assertThrows(DuplicateResourceException.class, () -> underTest.updateCustomerById(id, update));
+        verify(customerDoa, never()).updateCustomer(any());
+
+    }
+
+    @Test
+    void itWillThrowWhenNoChangeDetectedUpdateCustomerById() {
+        // given
+        Long id = 1L;
+        String actualName = "Updated Name";
+        String actualEmail = "updated@gmail.com";
+        Integer actualAge = 22;
+
+//        CustomerUpdateRequest update = mock(CustomerUpdateRequest.class);
+//        when(update.actualName()).thenReturn(actualName);
+//        when(update.actualEmail()).thenReturn(actualEmail);
+//        when(update.actualAge()).thenReturn(actualAge);
+        CustomerUpdateRequest update = new CustomerUpdateRequest(actualName, actualEmail, actualAge);
+
+        Customer existingCustomer = new Customer(actualName, actualEmail, actualAge);
+        when(customerDoa.selectCustomerById(id)).thenReturn(Optional.of(existingCustomer));
+//        when(customerDoa.existsCustomerWithEmail(actualEmail)).thenReturn(false);
+
+        // when
+        assertThrows(RequestValidationException.class, () -> underTest.updateCustomerById(id, update));
+        verify(customerDoa, never()).updateCustomer(any());
     }
 }
