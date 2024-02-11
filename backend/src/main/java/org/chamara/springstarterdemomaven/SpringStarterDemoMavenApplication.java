69f3ac98a1d5aaf8a08ab877aa3ca756f0016a58
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-11 06:53:57 +0530

    Add Docker Compose file and seed database with sample data

--- backend/src/main/java/org/chamara/springstarterdemomaven/SpringStarterDemoMavenApplication.java
+++ backend/src/main/java/org/chamara/springstarterdemomaven/SpringStarterDemoMavenApplication.java
@@ -1,7 +1,15 @@
 package org.chamara.springstarterdemomaven;
 
+import com.github.javafaker.Faker;
+import org.chamara.springstarterdemomaven.customer.Customer;
+import org.chamara.springstarterdemomaven.customer.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringStarterDemoMavenApplication {
@@ -10,21 +18,21 @@ public class SpringStarterDemoMavenApplication {
         SpringApplication.run(SpringStarterDemoMavenApplication.class, args);
     }
 
-//    @Bean
-//    CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
-//        return args -> {
-//            Faker faker = new Faker();
-//            String name;
-//            int age;
-//            String email;
-//            List<Customer> customers = new ArrayList<>();
-//            for (int i = 0; i < 10; i++) {
-//                name = faker.name().firstName();
-//                age = faker.number().numberBetween(20, 50);
-//                email = faker.internet().emailAddress();
-//                customers.add(new Customer(name, email, age));
-//            }
-//            customerRepository.saveAll(customers);
-//        };
-//    }
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
+        return args -> {
+            Faker faker = new Faker();
+            String name;
+            int age;
+            String email;
+            List<Customer> customers = new ArrayList<>();
+            for (int i = 0; i < 10; i++) {
+                name = faker.name().firstName();
+                age = faker.number().numberBetween(20, 50);
+                email = faker.internet().emailAddress();
+                customers.add(new Customer(name, email, age));
+            }
+            customerRepository.saveAll(customers);
+        };
+    }
 }
